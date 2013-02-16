ae4055193b3a4ac2cd6ce0a11d1c7ce544a611f7
Dan Ackroyd <Danack@basereality.com>
2013-02-16 14:42:36 +1100

    Removed new line, until we can figure out to remove extra brackets from require.

--- src/PHPToJavascript/CodeConverterState/REQUIRE.php
+++ src/PHPToJavascript/CodeConverterState/REQUIRE.php
@@ -7,7 +7,7 @@ class CodeConverterState_REQUIRE extends CodeConverterState{
 	function	processToken($name, $value, $parsedToken){
 
 		if($name == 'T_CONSTANT_ENCAPSED_STRING'){
-			$this->stateMachine->addJS('// Opening the require'.$value."\n");
+			$this->stateMachine->addJS('// Opening the require'.$value);
 
 			$this->changeToState(CONVERTER_STATE_DEFAULT);
 			$this->stateMachine->requireFile($value);
