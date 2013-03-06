a1cbed957933fffbb1a92942c6d4ce510a5c9193
Danack <Danack@basereality.com>
2013-03-06 15:58:05 +1100

    Removed trace - shouldn't be checked into repos with it on.

--- tests/examples.php
+++ tests/examples.php
@@ -71,7 +71,7 @@ function generateTestPage($convertedFiles){
 foreach($filesToConvert as $outputFilename =>  $inputFileList ){
 	$phpToJavascript = new PHPToJavascript\PHPToJavascript();
 
-	$phpToJavascript->setTrace(true);
+	//$phpToJavascript->setTrace(true);
 
 	$phpToJavascript->setEchoConversionFunction(PHPToJavascript\PHPToJavascript::$ECHO_TO_ALERT);
 
